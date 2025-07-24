efb4bcf4072f2d6c68614b1a071962505faa2b6c
edvaldo-65 <edvaldodentt@gmail.com>
2025-07-24 16:05:02 -0300

    :sparkle: feat - Enhancing the Highlights screen with the CustomScrollView and SliverToBoxAdapter widgets.

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -1,25 +1,37 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart' show BuildContext,
+CustomScrollView, EdgeInsets, Padding, SliverChildBuilderDelegate,
+SliverList, SliverToBoxAdapter, StatelessWidget, Text, Theme, Widget;
 import '../cardapio.dart';
 import '../components/highlight_item.dart';
 
 class Highlights extends StatelessWidget {
   const Highlights({super.key});
-   final List items = destaques;
+
+  final List items = destaques;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(
-        itemBuilder: (context, index){
-          return HighlightItem(
-              imageURI: items[index]["image"],
-              itemTitle: items[index]["name"],
-              itemPrice: items[index]["price"],
-              itemDescription: items[index]["description"]
-          );
-        },
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Text(
+              "Destaques",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return HighlightItem(
+                imageURI: items[index]["image"],
+                itemTitle: items[index]["name"],
+                itemPrice: items[index]["price"],
+                itemDescription: items[index]["description"],
+              );
+            }, childCount: items.length),
+          ),
+        ],
       ),
     );
   }
